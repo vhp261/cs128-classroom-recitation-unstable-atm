e2c2d94c9d58c775da8514060755d1135bf47442
vhp261 <pvh2617@gmail.com>
2025-10-03 10:38:04 -0500

    First commit

--- tests/tests.cc
+++ tests/tests.cc
@@ -61,6 +61,22 @@ TEST_CASE("Example: Simple widthdraw", "[ex-2]") {
   REQUIRE(sam_account.balance == 280.30);
 }
 
+TEST_CASE("Empty & Negative Withdraw") {
+  Atm atm;
+  REQUIRE_THROWS_AS(atm.WithdrawCash(213131, 2323, 30), std::invalid_argument);
+
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  REQUIRE_THROWS_AS(atm.WithdrawCash(12345678, 1234, -30), std::invalid_argument);
+  REQUIRE_THROWS_AS(atm.WithdrawCash(12345678, 1234, 400), std::runtime_error);
+}
+
+TEST_CASE("Deposit") {
+  Atm atm;
+  REQUIRE_THROWS_AS(atm.DepositCash(12345678, 123, 100.3), std::invalid_argument);
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  REQUIRE_THROWS_AS(atm.DepositCash(12345678, 1234, -300), std::invalid_argument);
+}
+
 TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   Atm atm;
   atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
@@ -73,4 +89,11 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
       "Deposit - Amount: $32000.00, Updated Balance: $72099.90");
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
+}
+
+TEST_CASE("PrintLedger") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  auto& transactions = atm.GetTransactions();
+  REQUIRE_THROWS_AS(atm.PrintLedger("./error.txt", 1234567, 123), std::invalid_argument);
 }
